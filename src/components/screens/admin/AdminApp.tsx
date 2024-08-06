9cdfd9cf3be494a730bbdd5912ec363b025671c9
Reddripill <reddripill@mail.ru>
2024-08-06 18:17:39 +0300

    add postlist for admin panel

--- src/components/screens/admin/AdminApp.tsx
+++ src/components/screens/admin/AdminApp.tsx
@@ -3,15 +3,17 @@ import { Admin, Resource } from "react-admin";
 import authProvider from "@/providers/authProvider";
 import dataProvider from "@/providers/dataProvider";
 import { CartCreate, CartEdit, CartList } from "./AdminCart";
+import { PostList } from "./AdminPost";
 const AdminPanel: React.FC = () => {
    return (
       <Admin dataProvider={dataProvider} authProvider={authProvider}>
          <Resource
-            name="cart"
+            name="carts"
             list={CartList}
             edit={CartEdit}
             create={CartCreate}
          />
+         <Resource name="posts" list={PostList} />
       </Admin>
    );
 };
