bdfc00bae93ac958a2cfd03803c05122ec22eff8
Reddripill <reddripillll@gmail.com>
2024-10-08 18:36:00 +0300

    add buttons to manipulate a schedule

--- src/components/screens/schedule/fullSchedule/FullSchedule.tsx
+++ src/components/screens/schedule/fullSchedule/FullSchedule.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useLayoutEffect } from "react";
 import FullCalendar from "@fullcalendar/react";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import ruLocale from "@fullcalendar/core/locales/ru";
@@ -6,11 +6,49 @@ import { scheduleEvents } from "../schedule.data";
 import FullScheduleEvent from "./FullScheduleEvent";
 import "./FullSchedule.scss";
 import FullScheduleSlotLabel from "./FullScheduleSlotLabel";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 const FullSchedule = () => {
    const scheduleRef = useRef<FullCalendar>(null);
-   const [eventSlotHeight, SetEventSlotHeight] = useState(100);
+   const [eventSlotHeight, setEventSlotHeight] = useState(100);
    const EVENT_GAP = 8;
+   const configAspectRatio = () => {
+      if (typeof window !== undefined) {
+         const width = window.innerWidth;
+         if (width > 1400) {
+            setEventSlotHeight(100);
+         } else if (width > 1024) {
+            setEventSlotHeight(140);
+         } else {
+            setEventSlotHeight(100);
+         }
+      }
+   };
+   const handlePrevClick = () => {
+      const calendarApi = scheduleRef.current?.getApi();
+      if (calendarApi) calendarApi.prev();
+   };
+   const handleNextClick = () => {
+      const calendarApi = scheduleRef.current?.getApi();
+      if (calendarApi) calendarApi.next();
+   };
+   useLayoutEffect(() => {
+      let throttled = false;
+      const handleResize = () => {
+         if (!throttled) {
+            configAspectRatio();
+            throttled = true;
+            setTimeout(function () {
+               throttled = false;
+            }, 250);
+         }
+      };
+      configAspectRatio();
+      window.addEventListener("resize", handleResize);
+      return () => {
+         window.removeEventListener("resize", handleResize);
+      };
+   }, []);
    return (
       <div className="full-schedule">
          <FullCalendar
@@ -27,7 +65,6 @@ const FullSchedule = () => {
             }}
             slotLabelFormat={{ hour: "2-digit", minute: "2-digit" }}
             slotLabelContent={(event) => {
-               // console.log("Slot event: ", event);
                return (
                   <FullScheduleSlotLabel
                      eventSlotHeight={eventSlotHeight}
@@ -58,6 +95,18 @@ const FullSchedule = () => {
                );
             }}
          />
+         <div
+            className="fc-schedule-button fc-schedule-button__prev"
+            onClick={handlePrevClick}
+         >
+            <ChevronLeft />
+         </div>
+         <div
+            className="fc-schedule-button fc-schedule-button__next"
+            onClick={handleNextClick}
+         >
+            <ChevronRight />
+         </div>
       </div>
    );
 };
