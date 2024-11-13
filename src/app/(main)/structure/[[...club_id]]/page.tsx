aa09640bd5dbb101604fe9b07c296ad4d69ab8c4
Reddripill <reddripillll@gmail.com>
2024-11-13 16:16:30 +0300

    fix structure page

--- src/app/(main)/structure/[[...club_id]]/page.tsx
+++ src/app/(main)/structure/[[...club_id]]/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export const metadata: Metadata = {
    title: "Структура организации",
 };
-const StructurePage = ({ params }: { params: { club_id?: string } }) => {
+const StructurePage = ({ params }: { params: { club_id?: string[] } }) => {
    return <StructureScreen club_id={params.club_id} />;
 };
 
